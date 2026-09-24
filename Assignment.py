eeb1331b44852a3649e75400513318190baf4bb7
Iris <palade.mariairis@gmail.com>
2026-09-24 14:23:31 -0400

    Refactor Assignment.py for clarity and formatting

--- Assignment.py
+++ Assignment.py
@@ -5,42 +5,48 @@
 # --------------------------------------------------------
 # #Question 1: Electricity Cost Calculator
 
-power=int(input("Enter the power in watts:"))
-time=int(input("Enter the number of hours:"))
-rate=float(input("Enter the electricity rate per kWh:"))
+power = int(input("Enter the power in watts:"))
+time = int(input("Enter the number of hours:"))
+rate = float(input("Enter the electricity rate per kWh:"))
+
 
 def calculate_cost(power, time, rate):
-    energy_cost=round((((power*time)/1000)*rate),2)
+    energy_cost = round((((power * time) / 1000) * rate), 2)
     return energy_cost
-energy_cost=calculate_cost(power, time, rate)
+
+
+energy_cost = calculate_cost(power, time, rate)
 print("Electricity cost:", calculate_cost(power, time, rate), "$")
 
-if energy_cost<1:
+if energy_cost < 1:
     print("Cost category: Low cost")
-elif energy_cost>1 and energy_cost<5:
+elif energy_cost > 1 and energy_cost < 5:
     print("Cost category: Moderate cost")
 else:
     print("Cost category: High cost")
 # --------------------------------------------------------
 # Question 2: Final Grade Calculator
 
-lab_grade=int(input("Enter the lab grade:"))
-midterm_grade=int(input("Enter the midterm exam grade:"))
-final_exam_grade=int(input("Enter the final exam grade:"))
+lab_grade = int(input("Enter the lab grade:"))
+midterm_grade = int(input("Enter the midterm exam grade:"))
+final_exam_grade = int(input("Enter the final exam grade:"))
+
 
 def calculate_grade(lab_grade, midterm_grade, final_grade):
-    final_grade=round((lab_grade*0.30)+(midterm_grade*0.30)+(final_grade*0.40),1)
+    final_grade = round((lab_grade * 0.30) + (midterm_grade * 0.30) + (final_grade * 0.40), 1)
     return final_grade
-final_grade=calculate_grade(lab_grade, midterm_grade, final_exam_grade)
+
+
+final_grade = calculate_grade(lab_grade, midterm_grade, final_exam_grade)
 print("Final grade:", final_grade)
 
-if final_grade>=90:
+if final_grade >= 90:
     print("Result: Excellent")
-elif final_grade>=80 and final_grade<90:
+elif final_grade >= 80 and final_grade < 90:
     print("Result: Very Good")
-elif final_grade>=70 and final_grade<80:
+elif final_grade >= 70 and final_grade < 80:
     print("Result: Good")
-elif final_grade>=60 and final_grade<70:
+elif final_grade >= 60 and final_grade < 70:
     print("Result: Satisfactory")
 else:
     print("Result: Needs Improvement")
